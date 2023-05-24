f69179b1e886d1fa714f7769bc50e6a91e280dda
Davoyesh <oyindola.esho@gmail.com>
2023-05-24 01:44:58 -0700

    Simple shell

--- main.h
+++ main.h
@@ -1,11 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#define _POSIX_C_SOURCE 200809L
-#define _GNU_SOURCE
+#ifndef SHELL_H
+#define SHELL_H
+#define MAX_COMMAND_LENGTH 100
 
-void execmd(char **argv);
-char *get_location(char *command);
+void read_command(char* command);
+void execute_command(char* command);
+
+#endif
--- shell.c
+++ shell.c
@@ -1,22 +1,63 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(int ac, char **argv)
-{
-	char *prompt = "(Eshell) $ ";
-	char *lineptr = NULL;
-	size_t n = 0;
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
 
-	/* declaring void variables */
-	(void)ac;
-	(void)argv;
+// Function to parse the command and arguments from user input
+void parse_command(char* input, char* command, char** arguments) {
+    char* token = strtok(input, " ");
+    strcpy(command, token);
+    int i = 0;
+    while (token != NULL && i < MAX_ARGUMENTS) {
+        arguments[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+    arguments[i] = NULL;
+}
 
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+// Function to execute the command
+void execute_command(char* command, char** arguments) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        // Child process
+        execvp(command, arguments);
+        // If execvp returns, there was an error
+        perror("Error executing command");
+        exit(1);
+    } else if (pid > 0) {
+        // Parent process
+        int status;
+        waitpid(pid, &status, 0);
+    } else {
+        // Fork failed
+        perror("Error forking process");
+        exit(1);
+    }
+}
 
-	free(lineptr);
-	return (0);
+int main(void) {
+    char input[MAX_COMMAND_LENGTH];
+    char command[MAX_COMMAND_LENGTH];
+    char* arguments[MAX_ARGUMENTS];
+
+    while (1) {
+        printf("shell$ ");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            // Handle end of file (Ctrl+D)
+            printf("\n");
+            break;
+        }
+        // Remove trailing newline character
+        input[strcspn(input, "\n")] = '\0';
+        parse_command(input, command, arguments);
+        execute_command(command, arguments);
+    }
+
+    return 0;
 }
